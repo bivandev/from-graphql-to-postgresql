3f78319e2149ccd9017c0fc0d0f7f5e270f24f49
Ivan <bvan@mail.ru>
2023-02-18 23:05:06 +0300

    add readme and refactoring

---
+++ README.md
@@ -0,0 +1,17 @@
+Project Title
+This project is a simple app that downloads data from a GraphQL API and stores it in a PostgreSQL container. The app is containerized using Docker for easy deployment and management.
+
+Prerequisites
+To run this app, you need to have Docker and Docker Compose installed on your machine.
+
+Installation
+Clone this repository to your local machine.
+Navigate to the project directory in your terminal.
+Run the following command to start the app: docker-compose up -d
+if you want see tables and count_rows showcase use this comand: docker-compose logs -f app
+
+Usage
+After starting the app, it will download data from the specified GraphQL API and store it in the PostgreSQL container.
+To access the data, you can use any PostgreSQL client and connect to the spacexdb database on port 5432 by this comand: 
+docker exec -it <db-container-name>  psql -U <username> -W <db-name> (default= 'docker exec -it py_db_1  psql -U username -W spacexdb')
+You will be prompted for the password (default='secret')
--- app/app.py
+++ app/app.py
@@ -1,13 +1,16 @@
 import requests
 import psycopg2
+import json
 from time import sleep
 
+# Download the data from the SpaceX GraphQL API
 def download_data(query):
     url = "https://spacex-production.up.railway.app/"
     response = requests.post(url, json={'query': query})
     data = response.json()
     return data
 
+# Store the data in the database
 def store_data(conn, data):
     cur = conn.cursor()
     for item in data['rockets']:
@@ -15,8 +18,8 @@ def store_data(conn, data):
             INSERT INTO rockets (rocket_id, rocket_name, rocket_type, active, boosters,cost_per_launch, country, first_flight) 
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
             ON CONFLICT DO NOTHING
-        """, (item['id'], item['name'], item['type'], item['active'], item['boosters'], 
-              item['cost_per_launch'], item['country'], item['first_flight'],))
+       """, (item['id'], item['name'], item['type'], item['active'], item['boosters'], 
+             item['cost_per_launch'], item['country'], item['first_flight'],))
   
     for item in data['launches']:
         cur.execute("""
@@ -33,113 +36,47 @@ def store_data(conn, data):
 
         conn.commit()
 
+# Load the JSON file with config data
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+# Creating tables in the database
 def create_db(conn):
     cur = conn.cursor()
-    cur.execute("""CREATE TABLE IF NOT EXISTS rockets (
-                    rocket_id VARCHAR(255) PRIMARY KEY,
-                    rocket_name VARCHAR(255) NOT NULL,
-                    rocket_type VARCHAR(255),
-                    active BOOLEAN,
-                    boosters INT,
-                    cost_per_launch INT,
-                    country VARCHAR(150),
-                    first_flight DATE
-                  );""")
-    cur.execute("""CREATE TABLE IF NOT EXISTS launches (
-                    launch_id VARCHAR(255) PRIMARY KEY,
-                    launch_date_utc VARCHAR(255),
-                    launch_success BOOLEAN,
-                    details TEXT,
-                    rocket_id VARCHAR(255),
-                    CONSTRAINT fk_rocket_id
-                      FOREIGN KEY (rocket_id)
-                      REFERENCES rockets(rocket_id)
-                  );""")
-    cur.execute("""CREATE TABLE IF NOT EXISTS missions (
-                    mission_id VARCHAR(255) PRIMARY KEY,
-                    mission_name VARCHAR(255) NOT NULL,
-                    launch_id VARCHAR(255),
-                    CONSTRAINT fk_launch_id
-                      FOREIGN KEY (launch_id)
-                      REFERENCES launches(launch_id)
-                  );""")
-
+    cur.execute(config['ct_rockets'])
+    cur.execute(config['ct_launches'])
+    cur.execute(config['ct_missions'])
     conn.commit()
-
-# Set the parameters
-host = 'db'
-port = '5432'
-user = 'username'
-password = 'secret'
-database = 'spacexdb'
-
-graph_query = """
-{
-    launches {
-      id
-      launch_date_utc
-      launch_success
-      details
-
-      mission_id
-      mission_name
-      
-      rocket {
-        rocket {
-          id
-        }
-      }
-    }
-    rockets {
-      active
-      boosters
-      cost_per_launch
-      country
-      first_flight
-      id
-      name
-      type
-    }
-}
-"""
-
-showcase_query="""
-        SELECT relname AS table_name, n_live_tup AS row_count
-        FROM pg_stat_user_tables
-        ORDER BY n_live_tup DESC;
-    """
 
 # Connect to the database
 def connection():
     conn = None
     while conn is None:
         try:
-            conn = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
+            conn = psycopg2.connect(database=config['database'], user=config['user'], 
+                                    password=config['password'], host=config['host'], port=config['port'])
         except psycopg2.OperationalError:
             sleep(5)
     return conn
 
+# Show tables and rows
 def showcases(conn):
     cur = conn.cursor()
-    cur.execute(showcase_query)
+    cur.execute(config['showcase'])
     rows = cur.fetchall()
     for row in rows:
         print(row)
     conn.commit()
 
+
 conn = connection()
 
-# Creating database
 create_db(conn)
 
-# Download the data from the SpaceX GraphQL API
-data = download_data(graph_query)
+data = download_data(config['graphql'])
 
-# Store the data in the database
 store_data(conn, data['data'])
 
-# Data showcases
 showcases(conn)
 
-# Close the connection
 conn.close()
